c3d85028327f417bc0f60bed47a22e3e7e2ef5d6
AT0MBE5T <atombest999@gmail.com>
2026-05-27 11:14:56 +0300

    t

--- RealEstateAgency.API/Program.cs
+++ RealEstateAgency.API/Program.cs
@@ -77,7 +77,16 @@ builder.Services.AddAuthentication(options =>
     {
         OnMessageReceived = context =>
         {
-            context.Token = context.Request.Cookies["accessToken"];
+            var accessToken = context.Request.Query["access_token"];
+
+            var path = context.HttpContext.Request.Path;
+
+            if (!string.IsNullOrEmpty(accessToken) &&
+                path.StartsWithSegments("/messageHub"))
+            {
+                context.Token = accessToken;
+            }
+
             return Task.CompletedTask;
         }
     };
