0ed557bbfd34e093024966ec6bb0dc2146dbb8b2
AT0MBE5T <atombest999@gmail.com>
2026-06-01 09:17:41 +0300

    test

--- RealEstateAgency.API/Controllers/AccountsController.cs
+++ RealEstateAgency.API/Controllers/AccountsController.cs
@@ -226,8 +226,8 @@ public class AccountsController(
         }
 
         var result = mapper.LoginRequestToResponse(loginRequest);
-        result.Id = response.UserId;
-        refreshService.SetRefreshToken(response.RefreshToken);
+        result.Id = response.UserId; 
+        //refreshService.SetRefreshToken(response.RefreshToken);
         result.AccessToken = response.AccessToken;
         result.RefreshToken = response.RefreshToken;
         
